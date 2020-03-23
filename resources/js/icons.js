10962080669fefc897845ebee999fa2c399e499e
Tyler Reed <tylernathanreed@gmail.com>
2020-03-23 11:21:03 -0500

    + Added software icon

--- resources/js/icons.js
+++ resources/js/icons.js
@@ -9,6 +9,7 @@ Nova.booting((Vue, router, store) => {
     Vue.component('icon-logout', require('./components/Logout'));
     Vue.component('icon-magic', require('./components/Magic'));
     Vue.component('icon-project-diagram', require('./components/ProjectDiagram'));
+    Vue.component('icon-software', require('./components/Software'));
     Vue.component('icon-sort', require('./components/Sort'));
     Vue.component('icon-tasks', require('./components/Tasks'));
 
